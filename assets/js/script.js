9a7984410727ee6f1a809d7fe3c5b318ba31f5b7
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-06-03 15:42:45 -0500

    Added timeblocks to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ let currentDate = dayjs().format("dddd MMMM D YYYY");
 $(document).ready(function () {
   $("#currentDay").append(currentDate);
 });
+
+const timeBlocksEl = $(".container");
+
+let createTimeBlock = function () {};
