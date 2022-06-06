096457ac8f353bf119f4818f342031a4a0bae4ff
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-06-06 16:14:39 -0500

    Finished application. Added clear button and persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,47 @@
 // Div holding time blocks
 const timeBlocksEl = $(".container");
-// Description Boxes
-const taskEl = $(".description");
-
+// Save buttons
+let saveBtn = $(".saveBtn");
+// Current date
 let currentDate = dayjs().format("dddd MMMM D YYYY");
-
-$(document).ready(function () {
-  $("#currentDay").append(currentDate);
-});
-
+// Current time (hr)
 let currentTime = dayjs().hour();
 
-taskEl.on("click", function (event) {
-  // Select the task clicked on
-  let taskChoosen = event.target;
-  // Take whatever text is already in the task
-  let taskText = taskChoosen.innerHTML;
+// Task Array
+let tasks = [];
 
-  $(this).replaceWith(`<textarea class="description">${taskText}</textarea>`);
+// Load in saved tasks from local storage
+const loadTasks = function () {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+  // If local storage is empty, replace with empty array
+  if (!tasks) {
+    tasks = [];
+  }
+  // Loop over objects in tasks array
+  for (i = 0; i < tasks.length; i++) {
+    // If empty object, replace with placeholder items
+    if (!tasks[i]) {
+      tasks[i] = {
+        time: i + 9,
+        task: "",
+      };
+    }
+    // Push time and task information to createTask function
+    createTask(parseInt(tasks[i].time), tasks[i].task);
+  }
+};
+
+// Create task using taskTime and taskText
+let createTask = function (taskTime, taskText) {
+  // Select proper textarea by targeting id
+  let taskHolder = $(`textarea[id=${taskTime}]`)[0];
+  // Add value to textarea using taskText passed into function
+  $(taskHolder).val(taskText);
+};
+
+// On document load, update current date
+$(document).ready(function () {
+  $("#currentDay").append(currentDate);
 });
 
 // Set background for each timeblock
@@ -40,4 +64,29 @@ let setBackground = function () {
   }
 };
 
-setBackground();
+// When save button is clicked...
+$(".saveBtn").on("click", function (event) {
+  // Find button's parent
+  let selectedTask = $(event.target).parent();
+  // Set variable to the dataset corresponding to button/task
+  selectedTask = selectedTask[0].dataset.task;
+  // Gather text within textarea
+  let taskText = $(event.target).prev().find("textarea").val();
+  // Set selected task's object array
+  tasks[selectedTask - 9] = {
+    time: selectedTask,
+    task: taskText,
+  };
+  // Save to local storage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+});
+
+// Clear saved tasks
+$("#clear").on("click", function () {
+  tasks = [];
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  window.location.reload();
+});
+
+setInterval(setBackground(), 60000);
+loadTasks();
